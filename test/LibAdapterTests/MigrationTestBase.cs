b453dd23d07521c70a484e87cb7cd619dd9150d6
Tomas Mūžas <muzastomas@gmail.com>
2020-04-18 19:23:13 +0300

    Add tests

---
+++ test/LibAdapterTests/MigrationTestBase.cs
@@ -0,0 +1,26 @@
+﻿using LibAdapter.Migrations;
+using LibAdapter.Visitors;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+
+namespace LibAdapterTests
+{
+    public class MigrationTestBase
+    {
+        public SyntaxTree PerformMigration(IMigration migration, string sourceCode)
+        {
+            var compilation = CSharpCompilation.Create("Code")
+                .AddReferences(MetadataReference.CreateFromFile(typeof(object).Assembly.Location));
+
+            var tree = CSharpSyntaxTree.ParseText(sourceCode);
+            tree = tree.WithRootAndOptions(new AnnotationVisitor().Visit(tree.GetRoot()), tree.Options);
+
+            compilation = compilation.AddSyntaxTrees(tree);
+
+            var context = new MigrationContext();
+            context.Populate(compilation, tree);
+
+            return migration.Apply(tree, context);
+        }
+    }
+}
---
+++ test/LibAdapterTests/RenameClassMigrationTests.cs
@@ -0,0 +1,46 @@
+﻿using LibAdapter.Migrations;
+using Xunit;
+
+namespace LibAdapterTests
+{
+    public class RenameClassMigrationTests : MigrationTestBase
+    {
+        [Fact]
+        public void Apply_TransformsSourceCodeCorrectly()
+        {
+            var source = @"
+            namespace Test
+            {
+                public class TestClass
+                {
+                }
+
+                public class Program
+                {
+                    var instance = new TestClass();
+                }
+            }
+            ";
+
+            var expected = @"
+            namespace Test
+            {
+                public class TestClass
+                {
+                }
+
+                public class Program
+                {
+                    var instance = new NewClass();
+                }
+            }
+            ";
+
+            var ast = PerformMigration(
+                new RenameClassMigration("Test.TestClass", "NewClass"),
+                source);
+
+            Assert.Equal(expected, ast.ToString());
+        }
+    }
+}
