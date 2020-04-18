8008724a151dedff42bb193c356d676a2ee93c10
Tomas Mūžas <muzastomas@gmail.com>
2020-04-18 16:43:10 +0300

    Clean up program.cs

--- Program.cs
+++ Program.cs
@@ -1,13 +1,11 @@
 ﻿using System; 
 using System.Collections.Generic;
 using System.IO;
-using System.Linq;
 using System.Reflection;
 using LibAdapter.Migrations;
 using LibAdapter.Visitors;
 using Microsoft.CodeAnalysis;
 using Microsoft.CodeAnalysis.CSharp;
-using Microsoft.CodeAnalysis.CSharp.Syntax;
 
 namespace LibAdapter
 {
@@ -15,11 +13,6 @@ namespace LibAdapter
     {
         static void Main(string[] args)
         {
-            #region Preparation
-            var actions = ChangeFileParser
-                .ParseFile(args[0])
-                .ToList();
-
             var projectPath = args[1];
             var mainDllPath = args[2];
 
@@ -51,42 +44,23 @@ namespace LibAdapter
                 var code = File.ReadAllText(filePath);
                 var tree = CSharpSyntaxTree.ParseText(code);
                 tree = tree.WithRootAndOptions(new AnnotationVisitor().Visit(tree.GetRoot()), tree.Options);
+
                 trees.Add(tree);
             }
 
             compilation = compilation.AddSyntaxTrees(trees);
-            var maps = new List<MigrationContext>();
 
             var watch = System.Diagnostics.Stopwatch.StartNew();
             foreach (var tree in trees)
             {
                 var map = new MigrationContext(tree);
                 map.PopulateFromCompilation(compilation);
-                maps.Add(map);
-            }
-            watch.Stop();
-            Console.WriteLine("Loading took:" + watch.ElapsedMilliseconds);
 
-            #endregion
-            
-            watch = System.Diagnostics.Stopwatch.StartNew();
-            foreach (var action in actions)
-            {
-                foreach (var map in maps)
-                {
-                    map.Root = (CompilationUnitSyntax) action.ToVisitor(map).Visit(map.Root);
-                }
+                var ast = new MyMigration().Apply(map.Root.SyntaxTree, map);
+                Console.WriteLine(ast.ToString());
             }
             watch.Stop();
             Console.WriteLine("Refactoring took:" + watch.ElapsedMilliseconds);
-
-            foreach (var map in maps)
-            {
-                Console.WriteLine(map.Root);
-                Console.WriteLine();
-                Console.WriteLine();
-                Console.WriteLine();
-            }
         }
     }
 }
