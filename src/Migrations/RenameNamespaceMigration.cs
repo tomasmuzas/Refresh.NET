6fd4b95e8b6fb43ec099fb47c59787b056ada3d9
Tomas Mūžas <m_tomas@inbox.lt>
2020-04-18 22:08:07 +0300

    Add RenameNamespace migration and tests

---
+++ src/Migrations/RenameNamespaceMigration.cs
@@ -0,0 +1,25 @@
+﻿using LibAdapter.Visitors.Namespace;
+using Microsoft.CodeAnalysis;
+
+namespace LibAdapter.Migrations
+{
+    public class RenameNamespaceMigration : IMigration
+    {
+        private readonly string _namespaceName;
+        private readonly string _newName;
+
+        public RenameNamespaceMigration(string namespaceName, string newName)
+        {
+            _namespaceName = namespaceName;
+            _newName = newName;
+        }
+
+        public SyntaxTree Apply(SyntaxTree initialAST, MigrationContext context)
+        {
+            var visitor = new RenameNamespaceVisitor(context, _namespaceName, _newName);
+            var ast = visitor.Visit(initialAST.GetRoot());
+
+            return ast.SyntaxTree;
+        }
+    }
+}
---
+++ test/LibAdapterTests/RenameNamespaceMigrationTests.cs
@@ -0,0 +1,30 @@
+﻿using LibAdapter.Migrations;
+using Xunit;
+
+namespace LibAdapterTests
+{
+    public class RenameNamespaceMigrationTests : MigrationTestBase
+    {
+        [Fact]
+        public void Apply_RenamesNamespaceInUsing()
+        {
+            var source = @"
+            using Test;
+            namespace Test
+            {
+            }";
+
+            var expected = @"
+            using NewNamespace;
+            namespace Test
+            {
+            }";
+
+            var refactoredAst = PerformMigration(
+                new RenameNamespaceMigration("Test", "NewNamespace"), 
+                source);
+
+            Assert.Equal(expected, refactoredAst.ToString());
+        }
+    }
+}
