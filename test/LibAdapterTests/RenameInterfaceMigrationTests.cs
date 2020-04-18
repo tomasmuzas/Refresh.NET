c1e8fbeed817a5b2d05f19e3688a7ad944c18805
Tomas Mūžas <m_tomas@inbox.lt>
2020-04-18 21:54:16 +0300

    Add RenameInterface migration and add extensive tests

---
+++ test/LibAdapterTests/RenameInterfaceMigrationTests.cs
@@ -0,0 +1,80 @@
+﻿using LibAdapter.Migrations;
+using Xunit;
+
+namespace LibAdapterTests
+{
+    public class RenameInterfaceMigrationTests : MigrationTestBase
+    {
+        [Fact]
+        public void Apply_RenamesInheritedInterface()
+        {
+            var source = @"
+            namespace Test
+            {
+                public interface ITestInterface{}
+
+                public class TestClass : ITestInterface
+                {
+                }
+
+                public class Program
+                {
+                    var instance = new TestClass();
+                }
+            }";
+
+            var expected = @"
+            namespace Test
+            {
+                public interface ITestInterface{}
+
+                public class TestClass : INewInterface
+                {
+                }
+
+                public class Program
+                {
+                    var instance = new TestClass();
+                }
+            }";
+
+            var refactoredAst = PerformMigration(
+                new RenameInterfaceMigration("Test.ITestInterface", "INewInterface"),
+                source);
+
+            Assert.Equal(expected, refactoredAst.ToString());
+        }
+
+        [Fact]
+        public void Apply_RenamesInterfaceTypedVariable()
+        {
+            var source = @"
+            namespace Test
+            {
+                public interface ITestInterface{}
+
+                public class Program
+                {
+                    ITestInterface instance;
+                }
+            }";
+
+            var expected = @"
+            namespace Test
+            {
+                public interface ITestInterface{}
+
+                public class Program
+                {
+                    INewInterface instance;
+                }
+            }";
+
+            var refactoredAst = PerformMigration(
+                new RenameInterfaceMigration("Test.ITestInterface", "INewInterface"),
+                source);
+
+            Assert.Equal(expected, refactoredAst.ToString());
+        }
+    }
+}
