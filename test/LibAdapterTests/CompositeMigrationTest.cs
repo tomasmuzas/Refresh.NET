e5aeb332c1634d5ca35295157374591db6ccf2b1
Tomas Mūžas <m_tomas@inbox.lt>
2020-04-18 20:05:00 +0300

    Add CompositeMigration test

---
+++ test/LibAdapterTests/CompositeMigrationTest.cs
@@ -0,0 +1,62 @@
+﻿using LibAdapter.Migrations;
+using Xunit;
+
+namespace LibAdapterTests
+{
+    public class CompositeMigrationTest : MigrationTestBase
+    {
+        [Fact]
+        public void Apply_TransformsSourceCodeCorrectly()
+        {
+            var source = @"
+            namespace Test
+            {
+                public class TestClass
+                {
+                    public void TestMethod(){}
+                }
+
+                public class Program
+                {
+                    public void Main()
+                    {
+                        var instance = new TestClass();
+                        instance.TestMethod();
+                    }
+                }
+            }";
+
+            var expected = @"
+            namespace Test
+            {
+                public class TestClass
+                {
+                    public void TestMethod(){}
+                }
+
+                public class Program
+                {
+                    public void Main()
+                    {
+                        NewClass instance = new NewClass();
+                        instance.NewMethod();
+                    }
+                }
+            }";
+
+            var refactoredAst = PerformMigration(
+                new CompositeMigration(
+                        new RenameClassMigration("Test.TestClass", "NewClass"),
+                        new RenameMethodMigration(
+                            new Method
+                            {
+                                Type = "Test.TestClass",
+                                Name = "TestMethod"
+                            },
+                            "NewMethod")),
+                    source);
+
+            Assert.Equal(expected, refactoredAst.ToString());
+        }
+    }
+}
