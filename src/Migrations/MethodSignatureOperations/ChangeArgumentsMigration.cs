2e31c49acc2b4015c43070c1128c62321c60c5a2
Tomas Mūžas <m_tomas@inbox.lt>
2020-04-19 23:14:12 +0300

    Implement ChangeArguments migration and add tests

---
+++ src/Migrations/MethodSignatureOperations/ChangeArgumentsMigration.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+using LibAdapter.Visitors.MethodSignatureOperations;
+using Microsoft.CodeAnalysis;
+
+namespace LibAdapter.Migrations.MethodSignatureOperations
+{
+    public class ChangeArgumentsMigration : IMigration
+    {
+        private readonly Method _method;
+        private readonly List<(Argument, int)> _arguments;
+
+        public ChangeArgumentsMigration(Method method, List<(Argument, int)> arguments)
+        {
+            _method = method;
+            _arguments = arguments;
+        }
+
+        public SyntaxTree Apply(SyntaxTree initialAST, MigrationContext context)
+        {
+            var visitor = new ChangeArgumentsVisitor(context, _method, _arguments);
+            var ast = visitor.Visit(initialAST.GetRoot());
+
+            return ast.SyntaxTree;
+        }
+    }
+}
---
+++ test/LibAdapterTests/ChangeArgumentsMigrationTests.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections.Generic;
+using LibAdapter.Migrations;
+using LibAdapter.Migrations.MethodSignatureOperations;
+using Xunit;
+
+namespace LibAdapterTests
+{
+    public class ChangeArgumentsMigrationTests : MigrationTestBase
+    {
+        [Fact]
+        public void Apply_TransformsSourceCodeCorrectly()
+        {
+            var source = @"
+            namespace Test
+            {
+                public class TestClass
+                {
+                    public void TestMethod(object obj){}
+                }
+
+                public class Program
+                {
+                    public void Main()
+                    {
+                        var instance = new TestClass();
+                        instance.TestMethod(new object());
+                    }
+                }
+            }";
+
+            var expected = @"
+            namespace Test
+            {
+                public class TestClass
+                {
+                    public void TestMethod(object obj){}
+                }
+
+                public class Program
+                {
+                    public void Main()
+                    {
+                        var instance = new TestClass();
+                        instance.TestMethod(new string());
+                    }
+                }
+            }";
+
+            var refactoredAst = PerformMigration(
+                new ChangeArgumentsMigration(
+                    new Method
+                    {
+                        Type = "Test.TestClass",
+                        Name = "TestMethod"
+                    },
+                    new List<(Argument argument, int position)>
+                    {
+                        (
+                            new Argument
+                            {
+                                DefaultValueExpression = "new string()"
+                            },
+                            1)
+                    }), 
+                source);
+
+            Assert.Equal(expected, refactoredAst.ToString());
+        }
+    }
+}
