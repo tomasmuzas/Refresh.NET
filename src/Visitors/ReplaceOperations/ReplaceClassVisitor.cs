5f5f54fdf0d41394da4c8017fecd66479ee48a5f
Tomas Mūžas <m_tomas@inbox.lt>
2020-04-20 16:30:06 +0300

    Add ReplaceClass migration

---
+++ src/Visitors/ReplaceOperations/ReplaceClassVisitor.cs
@@ -0,0 +1,87 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using LibAdapter.Migrations;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using static Microsoft.CodeAnalysis.CSharp.SyntaxFactory;
+
+namespace LibAdapter.Visitors.ReplaceOperations
+{
+    public class ReplaceClassVisitor : CSharpSyntaxRewriter
+    {
+        private readonly MigrationContext _context;
+        private readonly string _type; 
+        private readonly string _newType;
+        private readonly IEnumerable<Argument> _constructorArguments;
+
+        public ReplaceClassVisitor(
+            MigrationContext context,
+            string type,
+            string newType,
+            IEnumerable<Argument> constructorArguments)
+        {
+            _context = context;
+            _type = type;
+            _newType = newType;
+            _constructorArguments = constructorArguments;
+        }
+
+        public override SyntaxNode VisitObjectCreationExpression(ObjectCreationExpressionSyntax node)
+        {
+            node = (ObjectCreationExpressionSyntax)base.VisitObjectCreationExpression(node);
+            if (_context.GetNodeContainingClassType(node) == _type)
+            {
+                node = node.WithArgumentList(
+                    CreateArgumentList(
+                        _constructorArguments,
+                        node.ArgumentList.CloseParenToken.LeadingTrivia,
+                        node.ArgumentList.CloseParenToken.TrailingTrivia));
+            }
+
+            return node;
+        }
+
+        public override SyntaxNode VisitIdentifierName(IdentifierNameSyntax node)
+        {
+            node = (IdentifierNameSyntax)base.VisitIdentifierName(node);
+
+            if (_context.GetNodeType(node) == _type)
+            {
+                node = node.WithIdentifier(SyntaxFactory.Identifier(_newType)
+                    .WithTrailingTrivia(node.Identifier.TrailingTrivia)
+                    .WithLeadingTrivia(node.Identifier.LeadingTrivia));
+
+                node = node.CopyAnnotationsTo(node);
+            }
+
+            return node;
+        }
+
+        private ArgumentListSyntax CreateArgumentList(IEnumerable<Argument> arguments, SyntaxTriviaList leadingTrivia, SyntaxTriviaList trailingTrivia)
+        {
+            var argList = ArgumentList(
+                SeparatedList(arguments.Select(a =>
+                {
+                    var newIdentifier = IdentifierName(a.Type);
+                    newIdentifier = (IdentifierNameSyntax)new AnnotationVisitor().Visit(newIdentifier);
+
+                    _context.UpdateNodeType(newIdentifier, a.Type);
+
+                    if (!string.IsNullOrEmpty(a.DefaultValueExpression))
+                    {
+                        return Argument(ParseExpression(a.DefaultValueExpression));
+                    }
+                    else
+                    {
+                        return Argument(DefaultExpression(newIdentifier));
+                    }
+                })));
+
+            argList = argList.WithCloseParenToken(argList.CloseParenToken
+                .WithTrailingTrivia(trailingTrivia)
+                .WithLeadingTrivia(leadingTrivia));
+            return argList;
+        }
+    }
+}
---
+++ test/LibAdapterTests/ReplaceClassMigrationTests.cs
@@ -0,0 +1,98 @@
+﻿using LibAdapter.Migrations;
+using LibAdapter.Migrations.ReplaceOperations;
+using Xunit;
+
+namespace LibAdapterTests
+{
+    public class ReplaceClassMigrationTests : MigrationTestBase
+    {
+        [Fact]
+        public void Apply_ReplacesConstructorArguments()
+        {
+            var source = @"
+            namespace Test
+            {
+                public class TestClass
+                {
+                }
+
+                public class Program
+                {
+                    public void Main()
+                    {
+                        var instance = new TestClass();
+                    }
+                }
+            }";
+
+            var expected = @"
+            namespace Test
+            {
+                public class TestClass
+                {
+                }
+
+                public class Program
+                {
+                    public void Main()
+                    {
+                        OtherNamespace.NewClass instance = new OtherNamespace.NewClass(new object());
+                    }
+                }
+            }";
+
+            var refactoredAst = PerformMigration(
+                new ReplaceClassMigration(
+                    "Test.TestClass",
+                    "OtherNamespace.NewClass",
+                    new [] { 
+                        new Argument
+                        {
+                            Type = "object",
+                            DefaultValueExpression = "new object()"
+                        }}),
+                source);
+
+            Assert.Equal(expected, refactoredAst.ToString());
+        }
+
+        [Fact]
+        public void Apply_ReplacesClassTypedVariable()
+        {
+            var source = @"
+            namespace Test
+            {
+                public class TestClass
+                {
+                }
+
+                public class Program
+                {
+                    TestClass instance;
+                }
+            }";
+
+            var expected = @"
+            namespace Test
+            {
+                public class TestClass
+                {
+                }
+
+                public class Program
+                {
+                    Test.NewClass instance;
+                }
+            }";
+
+            var refactoredAst = PerformMigration(
+                new ReplaceClassMigration(
+                    "Test.TestClass", 
+                    "Test.NewClass",
+                    new Argument[]{}),
+                source);
+
+            Assert.Equal(expected, refactoredAst.ToString());
+        }
+    }
+}
